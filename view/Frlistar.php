c70a2be8fd4e633d851e7d2db5cb0376f1cadcc4
reginaldo <reginaldobrain@gmail.com>
2020-09-04 10:48:03 -0300

    array

--- view/Frlistar.php
+++ view/Frlistar.php
@@ -18,4 +18,10 @@ while($row = $result->fetch(PDO::FETCH_ASSOC)){
   
 }
 
+$lotofixa=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,233,24,25];
+
+/*$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
+$array2 = array("a" => "green", "b" => "yellow", "blue", "red");
+$result_array = array_intersect_assoc($array1, $array2);
+print_r($result_array);*/
 ?>
