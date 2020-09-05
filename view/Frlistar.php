ca923dc2e48293dc89f43a9dea0c57024156eae9
reginaldo miranda <reginaldobrain@gmail.com>
2020-09-05 18:00:46 -0300

    aaa

--- view/Frlistar.php
+++ view/Frlistar.php
@@ -8,18 +8,26 @@ require_once("../php/BEgrud.php");
 $conn = getconexao();
 
 $sql= "SELECT * FROM lotofacil order by 'concurso'";
+
 $result = $conn->prepare($sql);
 $result->execute();
-var_dump($sql);
 
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
     echo $row['concurso'].'  ';
     echo $row['dezena']."<br>";
-    
-  
+} 
+$array = array();
+$nof = mysql_num_fields($sql);
+while($linha = mysql_fetch_array($sql)){
+   for($i = 0; $i < $nof; $i++){
+       $nf = mysql_field_name($sql, $$i);
+       $array[][$nf] = $linha[$i];
+   }
 }
+print_r($array);
+    
 
-$lotofixa=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,233,24,25];
+//$lotofixa=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,233,24,25];
 
 
 /*$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
